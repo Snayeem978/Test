c69180a9c7fd650e1215c01b6946d793a2a133ff
Nayeem <snayeem978@gmail.com>
2023-12-30 12:00:09 +0000

    added the files

---
+++ javaScript.js
@@ -0,0 +1,28 @@
+
+/*function Ludu(min,max){
+   return Math.floor(Math.random()*max-min+1)+1;
+}
+
+console.log(Ludu(1,6));*/
+
+
+/*const students=["scj","sci","nayeem","akash"];
+console.log(students.sort());*/
+
+/*const number=[25,100,5,9,99];
+number.sort(function (a,b){
+  if(a===b){
+   break;
+  }
+});
+console.log();*/
+
+/*function isLeapYear(year){
+   if((year%400===0)||(year%4===0)&&(year%100!=0)){
+     console.log(`${year} is a leap year`);
+   }
+   else{
+      console.log(`${year}is not leap Year`);
+   }
+}
+isLeapYear(2008 ) ;*/
